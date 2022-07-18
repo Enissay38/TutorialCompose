c0a7d0496da1eabf4479d41a4223216351ce5f8e
Enissay38 <109359671+Enissay38@users.noreply.github.com>
2022-07-18 11:23:06 +0200

    add OutlinedButton
    use of :
    *  modifier = Modifier.weight(1f)
    * row and column

--- app/src/main/java/com/example/listtuto/MainActivity.kt
+++ app/src/main/java/com/example/listtuto/MainActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,19 +35,23 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 }
 
 @Composable
-fun Greeting(name: String) {
+private fun Greeting(name: String) {
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxWidth()
-        ) {
-            Text(text = "Hello,")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show More")
+            }
         }
+        
     }
 }
 
